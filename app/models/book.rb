6974194647d1b1f4e50ca8931429ea0f1575dc0f
Lazaro Nixon <lazaronixon@hotmail.com>
2026-03-21 02:55:09 -0300

    cosmetic

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,6 @@
 class Book < ApplicationRecord
   has_many :borrowings, dependent: :destroy
 
-  validates :copies, numericality: { greater_than: 0 }
-
   scope :reverse_chronologically, -> { order created_at: :desc, id: :desc }
   scope :chronologically,         -> { order created_at: :asc,  id: :asc  }
   scope :search, ->(query) { where("LOWER(title || ' ' || author || ' ' || genre) LIKE ?", "%#{query.downcase}%") }
